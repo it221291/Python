cc09d077bf285affc6d7a2af4ae0036b3a526b14
D. Sanjana <120377977+it221291@users.noreply.github.com>
2023-04-26 14:55:44 +0530

    Add files via upload

---
+++ convert_to_array.py
@@ -0,0 +1,8 @@
+import numpy as np
+my_list=[1,2,3,4,5]
+print("list to array:")
+print(np.asarray(my_list))
+my_tuple = ([8, 4, 6], [1, 2, 3])
+print("Tuple to array: ")
+print(np.asarray(my_tuple))
+
